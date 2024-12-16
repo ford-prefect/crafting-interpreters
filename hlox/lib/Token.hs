6c703593e618a958edd3a481d5173f64da6e1f87
Arun Raghavan <arun@asymptotic.io>
2024-12-15 21:58:13 -0500

    hlox: Add token types as a library

---
+++ hlox/lib/Token.hs
@@ -0,0 +1,52 @@
+module Token where
+
+data TokenType
+  -- Single-character tokens
+  = LeftParen
+  | RightParen
+  | LeftBrace
+  | RightBrace
+  | Comma
+  | Dot
+  | Minus
+  | Plus
+  | Semicolon
+  | Slash
+  | Star
+  -- One-or-two-character tokens
+  | Bang
+  | BangEqual
+  | Equal
+  | Greater
+  | GreaterEqual
+  | Less
+  | LessEqual
+  -- Literals
+  | Identifier String
+  | Str String
+  | Num Double
+  -- Keywords
+  | And
+  | Class
+  | Else
+  | False
+  | Fun
+  | For
+  | If
+  | Nil
+  | Or
+  | Print
+  | Return
+  | Super
+  | True
+  | Var
+  | While
+  -- That's it
+  | Eof
+
+data Token = Token {
+  tokenType :: TokenType,
+  tokenLexeme :: String,
+  -- literal :: is embedded in the TokenType
+  tokenLine :: Int
+}
